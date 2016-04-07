179c97827a2843247e82492396fb944a91054acd
JBaba <naimish101.viradia@gmail.com>
2016-04-06 23:54:25 -0400

    functions

--- hello/functions.go
+++ hello/functions.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 )
 
+// function return multipale values
 func power(name string) (int,bool) {
 	return 0,false
 }
 
+// functions argument same type
+func add(a,b int) int {
+	sum := a+b
+	return sum
+}
+
 func main() {
 	fmt.Println("Its functions.go file")
+
+	flag := true
 
 	// Two return can be saved like this
 	value, flag := power("naimish")
@@ -17,5 +26,14 @@ func main() {
 	if flag == false {
 		fmt.Println("value:",value," flag:",flag)
 	}
+
+	// we only care about one value then use '_'
+	_, flag2 := power("vivek")
+
+	if flag2 == false {
+		fmt.Println("value:",value," flag:",flag2)
+	}
+
+	fmt.Println("Sum:",add(1,2))
 
 }
