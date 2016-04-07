772632ef8fd84457fa471d348409ef4f888e3379
JBaba <naimish101.viradia@gmail.com>
2016-04-06 22:51:38 -0400

    multi declare var

--- hello/Variables.go
+++ hello/Variables.go
@@ -5,8 +5,29 @@ import (
 )
 
 func main(){
+	
+	// Diclare variable using types
 	var power int
-	fmt.Printf("Default value %d\n",power)
+	var flag bool
+	var txt string
+	
+	fmt.Printf("Default value for int %d\n",power)
+	fmt.Printf("Default value for booleans %d\n",flag)
+	fmt.Printf("Default value for string %d\n",txt)
+	
 	power = 9
+	
 	fmt.Printf("It's over %d\n",power)
+	
+	// Single diclare variable without using type
+	x := 50
+	
+	fmt.Printf("It's over %d\n",x)
+	
+	// multiple variable declaration
+	name, y := "Naimish", 5
+	// %s is used to print name(string) 
+	// %d is used to print x(int)
+	
+	fmt.Printf("%s's age is %d\n",name,y)
 }
