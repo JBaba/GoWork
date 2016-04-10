2e775dc4d2933b7184f0d2e7d5b4ba7a2cfd1387
JBaba <naimish101.viradia@gmail.com>
2016-04-10 18:48:34 -0400

    Composition example added

---
+++ hello/composition.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct{
+	Name string
+}
+
+func (p *Person) Print(){
+	fmt.Println("Name of student:",p.Name)
+}
+
+type Student struct{
+	*Person
+	marks int
+}
+
+func main() {
+	naimish := &Student{
+		Person: &Person{
+			Name: "Naimish",
+		},
+		marks: 20,
+	}
+
+	naimish.Print()
+}
+
