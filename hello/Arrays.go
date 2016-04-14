219a5ab668df11e4afd759d0d343d070462a6c3d
JBaba <naimish101.viradia@gmail.com>
2016-04-14 13:35:59 -0400

    slice ex added

--- hello/Arrays.go
+++ hello/Arrays.go
@@ -23,5 +23,43 @@ func main() {
 	for index, value := range score_1 {
 		fmt.Println(index," : ",value)
 	}
-}
 
+	//slice with length of 5  capacity 5
+	score_2 := make([]int,5)
+	fmt.Println(score_2)
+
+	//slice with length of 0 capacity 5
+	score_3 := make([]int,0,5)
+	fmt.Println(score_3)
+
+	// this will crash due to size zero
+	//score_3[0] = 9033
+	score_3 = append(score_3,5)
+	fmt.Println(score_3)
+
+	scores_4 := make([]int, 0, 10)
+	scores_4 = scores_4[0:6]
+	scores_4[5] = 9033
+	fmt.Println(scores_4)
+
+	//dynemically expanding array
+	score_5 := make([]int,0,5)
+	c := cap(score_5)
+	fmt.Println("cap:",c)
+	fmt.Println(score_5)
+
+	for i := 0; i < 7; i++ {
+		score_5 = append(score_5,i)
+
+		if cap(score_5) != c {
+			c = cap(score_5)
+			fmt.Println("cap:",c)
+			fmt.Println(score_5)
+		}
+	}
+
+	//append
+	score_6 := make([]int,5)
+	score_6 = append(score_6,9332)
+	fmt.Println(score_6)
+}
