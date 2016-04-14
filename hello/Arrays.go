5f7961d1a7b3f48e20b3ae0bb128f8c6ca0d8563
JBaba <naimish101.viradia@gmail.com>
2016-04-13 23:26:49 -0400

    for loop ex

--- hello/Arrays.go
+++ hello/Arrays.go
@@ -8,5 +8,20 @@ func main() {
 	var score [10]int
 	score[0] = 10
 	fmt.Println(score)
+
+	score_1 := [4]int{10,22,31,45}
+	fmt.Println(score_1)
+	// Use of len() func
+	fmt.Println("Length of socre_1:",len(score_1))
+
+	//for loop
+	for i := 0; i < len(score_1); i++ {
+		fmt.Println(score_1[i])
+	}
+
+	//for loop
+	for index, value := range score_1 {
+		fmt.Println(index," : ",value)
+	}
 }
 
