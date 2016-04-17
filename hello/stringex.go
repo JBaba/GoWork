f2969c52f20720c44ac695d69c7bc53db8f77761
JBaba <naimish101.viradia@gmail.com>
2016-04-17 13:25:10 -0400

    removeAtindex

--- hello/stringex.go
+++ hello/stringex.go
@@ -9,7 +9,25 @@ func main() {
 	text := "the spice must flow"
 	num := strings.Index(text[5:]," ")
 	fmt.Println(num)
+	// number to the ends
 	fmt.Println(text[5:])
+	// to the number text
+	fmt.Println(text[:5])
 	fmt.Println(strings.Index(text[5:],"p"))
+
+	score := []int{1,2,3,4,5,6,7,8,9}
+	fmt.Println(score[:len(score)-1])
+
+	removeAtIndex(score,2)
+}
+
+func removeAtIndex(source []int,index int) []int {
+	lastIndex := len(source) - 1
+	// swap the last value and the value we want to remove
+	fmt.Println(source)
+	source[index],source[lastIndex] = source[lastIndex],source[index]
+	fmt.Println(source)
+
+	return source[:lastIndex]
 }
 
